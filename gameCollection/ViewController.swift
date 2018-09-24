4af414f713e87ed123bef9de68399dcd626babc7
leon <leon@leons-mac.local>
2018-09-23 23:14:24 -0400

    Initial Commit

---
+++ gameCollection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameCollection
+//
+//  Created by leon on 9/23/18.
+//  Copyright © 2018 leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
